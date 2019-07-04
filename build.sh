7dbe6c7934d819d2b8a7d150de6ec2909ff0b166
Denys Vitali <denys@denv.it>
2019-07-04 18:44:56 +0200

    Use xmlstartlet

--- build.sh
+++ build.sh
@@ -6,18 +6,15 @@ SOFTWARE="jira-core"
 # Get latest Jira Version
 echo "Getting latest version feed..."
 RSS_FILE=$(curl https://my.atlassian.com/download/feeds/${SOFTWARE}.rss)
-VERSIONS=$(echo "$RSS_FILE" | xmllint --xpath '/rss/channel/item/guid/text()' -)
+VERSIONS=$(echo "$RSS_FILE" | xmlstarlet sel -t -v '/rss/channel/item/guid/text()' -)
 LINUX_BIN=$(echo "$VERSIONS" | grep '\.bin')
 CURRENT_VERSION=$(echo "$LINUX_BIN" | sed -e "s@https://www.atlassian.com/software/${SOFTWARE/-core/}/downloads/binary/atlassian-$SOFTWARE-@@" -e 's/-x64.bin//')
-
 CURRENT_MAJOR=$(echo "$CURRENT_VERSION" | cut -d '.' -f 1)
 CURRENT_MINOR=$(echo "$CURRENT_VERSION" | cut -d '.' -f 2)
 CURRENT_PATCH=$(echo "$CURRENT_VERSION" | cut -d '.' -f 3)
 
-echo $RSS_FILE
 
 echo "Current version: ${CURRENT_VERSION}"
-
 echo "Current Major: ${CURRENT_MAJOR}"
 
 docker build --build-arg VERSION=${CURRENT_VERSION} -t dvitali/${IMAGE_NAME}:${CURRENT_VERSION} .

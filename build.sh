49243fcf2752c508ab8eb3bed051b8855ff829b5
Denys Vitali <denys@denv.it>
2019-07-04 18:25:19 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -5,14 +5,16 @@ SOFTWARE="jira-core"
 
 # Get latest Jira Version
 echo "Getting latest version feed..."
-RSS_FILE=$(curl https://my.atlassian.com/download/feeds/$SOFTWARE.rss)
+RSS_FILE=$(curl https://my.atlassian.com/download/feeds/${SOFTWARE}.rss)
 VERSIONS=$(echo "$RSS_FILE" | xmllint --xpath '/rss/channel/item/guid/text()' -)
 LINUX_BIN=$(echo "$VERSIONS" | grep '\.bin')
-CURRENT_VERSION=$(echo "$LINUX_BIN" | sed -e "s@https://www.atlassian.com/software/${SOFTWARE/core/}/downloads/binary/atlassian-$SOFTWARE-@@" -e 's/-x64.bin//')
+CURRENT_VERSION=$(echo "$LINUX_BIN" | sed -e "s@https://www.atlassian.com/software/${SOFTWARE/-core/}/downloads/binary/atlassian-$SOFTWARE-@@" -e 's/-x64.bin//')
 
 CURRENT_MAJOR=$(echo "$CURRENT_VERSION" | cut -d '.' -f 1)
 CURRENT_MINOR=$(echo "$CURRENT_VERSION" | cut -d '.' -f 2)
 CURRENT_PATCH=$(echo "$CURRENT_VERSION" | cut -d '.' -f 3)
+
+echo $RSS_FILE
 
 echo "Current version: ${CURRENT_VERSION}"
 

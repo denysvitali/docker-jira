b598ba598b00f92d75162ce8d8f145500226e746
Denys Vitali <denys@denv.it>
2020-05-21 15:03:02 +0200

    fix(build): Improve Shell Script, fix multi-version matching

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -e
 IMAGE_NAME="jira"
 SOFTWARE="jira-core"
 
 BUILD_EAP=0
 
 function usage(){
-  echo "Usage $1 [-e]"
+  echo "Usage $0 [-e]"
   exit 0
 }
 
@@ -40,7 +41,7 @@ fi
 LINUX_BIN=$(echo -n "$VERSIONS" | grep '\.bin$')
 LINUX_BIN=$(echo -e "$LINUX_BIN" | grep $SOFTWARE)
 
-CURRENT_VERSION=$(echo "$LINUX_BIN" | sed -e "s@https://www.atlassian.com/software/${SOFTWARE/-core/}/downloads/binary/atlassian-$SOFTWARE-@@" -e 's/-x64.bin//')
+CURRENT_VERSION=$(echo "$LINUX_BIN" | sed -e "s@https://www.atlassian.com/software/${SOFTWARE/-core/}/downloads/binary/atlassian-$SOFTWARE-@@" -e 's/-x64.bin//' | head -n 1)
 CURRENT_MAJOR=$(echo "$CURRENT_VERSION" | cut -d '.' -f 1)
 CURRENT_MINOR=$(echo "$CURRENT_VERSION" | cut -d '.' -f 2)
 CURRENT_PATCH=$(echo "$CURRENT_VERSION" | cut -d '.' -f 3)
@@ -49,28 +50,29 @@ CURRENT_PATCH=$(echo "$CURRENT_VERSION" | cut -d '.' -f 3)
 echo "Current version: ${CURRENT_VERSION}"
 echo "Current Major: ${CURRENT_MAJOR}"
 echo "Current Minor: ${CURRENT_MINOR}"
+echo "Current Patch: ${CURRENT_PATCH}"
 
 if [ $BUILD_EAP -eq 1 ]; then
 	docker build \
 	--build-arg "VERSION=${CURRENT_VERSION}" \
-	--build-arg "EAP=1" -t dvitali/${IMAGE_NAME}:${CURRENT_VERSION} .
-	docker tag dvitali/${IMAGE_NAME}:${CURRENT_VERSION} dvitali/${IMAGE_NAME}:eap
-	docker tag dvitali/${IMAGE_NAME}:${CURRENT_VERSION} dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}-eap
-	docker tag dvitali/${IMAGE_NAME}:${CURRENT_VERSION} dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}.${CURRENT_MINOR}-eap
-	docker tag dvitali/${IMAGE_NAME}:${CURRENT_VERSION} dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}.${CURRENT_MINOR}.${CURRENT_PATCH}-eap
+	--build-arg "EAP=1" -t "dvitali/${IMAGE_NAME}:${CURRENT_VERSION}" .
+	docker tag "dvitali/${IMAGE_NAME}:${CURRENT_VERSION}" "dvitali/${IMAGE_NAME}:eap"
+	docker tag "dvitali/${IMAGE_NAME}:${CURRENT_VERSION}" "dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}-eap"
+	docker tag "dvitali/${IMAGE_NAME}:${CURRENT_VERSION}" "dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}.${CURRENT_MINOR}-eap"
+	docker tag "dvitali/${IMAGE_NAME}:${CURRENT_VERSION}" "dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}.${CURRENT_MINOR}.${CURRENT_PATCH}-eap"
 
-	docker push dvitali/${IMAGE_NAME}:${CURRENT_VERSION}
-	docker push dvitali/${IMAGE_NAME}:eap
-	docker push dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}-eap
-	docker push dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}.${CURRENT_MINOR}-eap
-	docker push dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}.${CURRENT_MINOR}.${CURRENT_PATCH}-eap
+	docker push "dvitali/${IMAGE_NAME}:${CURRENT_VERSION}"
+	docker push "dvitali/${IMAGE_NAME}:eap"
+	docker push "dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}-eap"
+	docker push "dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}.${CURRENT_MINOR}-eap"
+	docker push "dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}.${CURRENT_MINOR}.${CURRENT_PATCH}-eap"
 else
-	docker build --build-arg VERSION=${CURRENT_VERSION} -t dvitali/${IMAGE_NAME}:${CURRENT_VERSION} .
-	docker tag dvitali/${IMAGE_NAME}:${CURRENT_VERSION} dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}
-	docker tag dvitali/${IMAGE_NAME}:${CURRENT_VERSION} dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}.${CURRENT_MINOR}
-	docker tag dvitali/${IMAGE_NAME}:${CURRENT_VERSION} dvitali/${IMAGE_NAME}:latest
+	docker build --build-arg VERSION="${CURRENT_VERSION}" -t "dvitali/${IMAGE_NAME}:${CURRENT_VERSION}" .
+	docker tag "dvitali/${IMAGE_NAME}:${CURRENT_VERSION}" "dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}"
+	docker tag "dvitali/${IMAGE_NAME}:${CURRENT_VERSION}" "dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}.${CURRENT_MINOR}"
+	docker tag "dvitali/${IMAGE_NAME}:${CURRENT_VERSION}" "dvitali/${IMAGE_NAME}:latest"
 
-	docker push dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}
-	docker push dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}.${CURRENT_MINOR}
-	docker push dvitali/${IMAGE_NAME}:latest
+	docker push "dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}"
+	docker push "dvitali/${IMAGE_NAME}:${CURRENT_MAJOR}.${CURRENT_MINOR}"
+	docker push "dvitali/${IMAGE_NAME}:latest"
 fi
